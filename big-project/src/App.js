f096ab5cbbad3944080acfa6993cad88a766673d
TLRKiliann <philogenie@protonmail.com>
2022-05-29 18:18:46 +0200

    new component added

--- big-project/src/App.js
+++ big-project/src/App.js
@@ -21,18 +21,8 @@ function App() {
     <div>
       <DarkFunc id={darkMode} darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> 
       
-      <MultiPage id={}/>
+      <MultiPage />
 
-    <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/home" component={Home} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contact/>} />
-      <Route path='/blogs' element={<Blogs/>} />
-      <Route path='/sign-up' element={<SignUp />} />
-    </Routes>
-    </Router>
 
     </div>
 
