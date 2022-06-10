0e0cde63851d9e9c66c92df16be6c0f69e4ebd05
TLRKiliann <philogenie@protonmail.com>
2022-06-10 12:18:40 +0200

    sass setup and file configuration not ok

--- big-project/src/Pages/index.js
+++ big-project/src/Pages/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import "./style.css"
+
+import "./style.css";
+//import "./index.scss";
 
 const Home = () => {
+
   return (
     <div className="about--div">
-      <h1>Home</h1>
+      
+      <div>
+        <h1>Home</h1>
+      </div>
+
+      <div>
+        <h1>
+          <span>L</span>
+          <span>a</span>
+          <span>s</span>
+          <span>e</span>
+          <span>r</span>
+          <span>&nbsp;</span>
+          <span>T</span>
+          <span>e</span>
+          <span>x</span>
+          <span>t</span>
+        </h1>
+      </div>
+
     </div>
   );
 };
